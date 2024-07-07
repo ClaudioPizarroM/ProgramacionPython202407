6b3c71fe6ec70b148ce9ddfbcb57408e7c2aed4b
ClaudioPizarroM <claudio.pi.me28@gmail.com>
2024-07-07 17:59:28 +0000

    primera carga modulo1

---
+++ Modulo1/scripts/hola.py
@@ -0,0 +1,12 @@
+# comentario de codigo 
+# los comentarios de codigo son para documentar el codigo y no se ejecutan. 
+#control + s para guardar 
+print("Hola mundo")
+
+
+#comandos de terminal
+#cd para cambiar de directorio
+#ls para listar los archivos
+#pwd para saber en que directorio estoy
+#clear limpiar la terminal
+# python hola.py para ejecutar el archivo
